9ed0366409a2036061b6d96967f5dba623d5dc4e
melan <melanieyupanqui44@gmail.com>
2023-05-09 22:24:14 -0400

    el https

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('eventsphoto-production.up.railway.app'),
+    'asset_url' => env('https://eventsphoto-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
